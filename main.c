fd2f5e4656679eebbd8c45ae02c8e2a5da8ab9fb
Darren Ng <Un1Gfn@gmail.com>
2022-03-19 01:44:07 +0800

    meson
    Signed-off-by: Darren Ng <Un1Gfn@gmail.com>

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 static void draw(GtkApplication *const app){
 
   GtkWidget *const window=gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window),"Title");
+  gtk_window_set_title(GTK_WINDOW(window),"Qu}9^/");
   gtk_window_set_default_size(GTK_WINDOW(window),200,200);
 
   // GtkWidget *const button=gtk_button_new_with_label("button_label");
@@ -30,7 +30,7 @@ static void draw(GtkApplication *const app){
   gtk_widget_show(window);
 }
 
-static void shutdown(GtkApplication *const app,const gconstpointer garbage){
+static void shutdown(GtkApplication *const app, const gconstpointer garbage){
   g_assert(app&&!garbage);
   g_print("shutdown\n");
   GList *l=gtk_application_get_windows(app);
@@ -41,7 +41,7 @@ static void shutdown(GtkApplication *const app,const gconstpointer garbage){
   // gtk_window_destroy(win);
 }
 
-static void activate(GtkApplication *const app,const gconstpointer garbage){
+static void activate(GtkApplication *const app, const gconstpointer garbage){
   g_assert(app&&!garbage);
   draw(app);
 }
@@ -55,21 +55,25 @@ static void activate(GtkApplication *const app,const gconstpointer garbage){
 int main(const int argc,char **const argv){
 
   if(!gtk_init_check()){
-    printf("fall back to textual interface\n");
+    printf("falling back to textual interface\n");
     return 0;
   }
 
   // https://en.wikipedia.org/wiki/Reverse_domain_name_notation
   g_assert(g_application_id_is_valid(APPID));
 
-  // No command line args
+  // (A) No command line args
   GtkApplication *app=gtk_application_new(APPID,G_APPLICATION_FLAGS_NONE);
   g_signal_connect(app,"shutdown",G_CALLBACK(shutdown),NULL);
   g_signal_connect(app,"activate",G_CALLBACK(activate),NULL);
-  g_set_prgname("floating"); // ~/.i3/conf/00-forwindow // https://unix.stackexchange.com/questions/521019/specifying-the-wm-class-of-a-program
+
+  // ~/.i3/conf/00-forwindow
+  // https://unix.stackexchange.com/questions/521019/specifying-the-wm-class-of-a-program
+  g_set_prgname("floating");
+
   g_assert(0==g_application_run(G_APPLICATION(app),0,NULL)); // Invalid read of size ...
 
-  // Command line args e.g. --gapplication-app-id
+  // (B) Command line args e.g. --gapplication-app-id
   // gio/stable/GApplication.html#g-application-run
   // GtkApplication *app=gtk_application_new(APPID,G_APPLICATION_CAN_OVERRIDE_APP_ID|G_APPLICATION_HANDLES_COMMAND_LINE);
   // g_signal_connect(app,"command-line",G_CALLBACK(command_line),NULL);
